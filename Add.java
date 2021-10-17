8315f86d303c7a114dfaf7cae2461a62faae3b3e
S230102 <91896750+S230102@users.noreply.github.com>
2021-10-17 15:14:46 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,15 @@
+public class Project3 {
+
+	public static void main(String[] args) {
+          int a=10,b=5;
+          int sum=a+b;
+          System.out.println(sum);
+          int sub=a-b;
+          System.out.println(sub);
+          int mul=a*b;
+          System.out.println(mul);
+          int div=a/b;
+          System.out.println(div);
+	}
+
+}
